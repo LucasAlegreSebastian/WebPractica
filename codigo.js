1d35a7166373eaa81e385b5b685ad8b30345042d
Lucas Alegre <lucasalegresebas@gmail.com>
2025-04-26 21:25:14 -0300

    Mensaje de alerta para seguir sumando funciones a la pagina

--- codigo.js
+++ codigo.js
@@ -1,1 +1,1 @@
-alert("Bienvenido a mi página");
+alert("Bienvenido a mi página✅\nMenú❌\nPresentación inicial con foto.❌\nInformacion General ❌\nConocimientos❌\nProyectos❌\nRedes❌\nFormulario de contacto.❌");
